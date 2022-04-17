852905ea57ddc8e7ddc6a889d5b1acb5d8c97aff
Sabin Acharya <sabinacharya444@gmail.com>
2022-04-18 08:53:44 +1000

    passing object as props

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,23 +7,23 @@ export default function Card(props){
 console.log(props)
      let cardStatus
      console.log(cardStatus)
-     if (props.openSpots === 0) {
+     if (props.data.openSpots === 0) {
         cardStatus = "SOLD OUT"         
-     }else if(props.location === "Online"){
+     }else if(props.data.location === "Online"){
         cardStatus = "ONLINE"
      }
     return(
         <div className="card">
             {cardStatus && <div className="card--status">{cardStatus}</div>}
-            <img className="card--image" src={props.img}/>
+            <img className="card--image" src={props.data.coverImg}/>
             <div className="card--ratings">
                 <img className="rating-icon" src="../images/star.png"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>       
+                <span>{props.data.stats.rating}</span>
+                <span className="gray">({props.data.stats.reviewCount}) • </span>
+                <span className="gray">{props.data.location}</span>       
             </div>    
             <p>{props.title}</p> 
-            <p><span className="bold">From {props.price}</span> / person</p>   
+            <p><span className="bold">From {props.data.price}</span> / person</p>   
         </div>
     )
 }
