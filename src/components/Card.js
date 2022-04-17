f6b24c87a67d0817b2cd4f2b32cf8e8dfa949d06
Sabin Acharya <sabinacharya444@gmail.com>
2022-04-18 09:03:43 +1000

    spread object as props {...data}

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,24 +6,24 @@ export default function Card(props){
 
 console.log(props)
      let cardStatus
-     console.log(cardStatus)
-     if (props.data.openSpots === 0) {
+     console.log(props)
+     if (props.openSpots === 0) {
         cardStatus = "SOLD OUT"         
-     }else if(props.data.location === "Online"){
+     }else if(props.location === "Online"){
         cardStatus = "ONLINE"
      }
     return(
         <div className="card">
             {cardStatus && <div className="card--status">{cardStatus}</div>}
-            <img className="card--image" src={props.data.coverImg}/>
+            <img className="card--image" src={props.coverImg}/>
             <div className="card--ratings">
                 <img className="rating-icon" src="../images/star.png"/>
-                <span>{props.data.stats.rating}</span>
-                <span className="gray">({props.data.stats.reviewCount}) • </span>
-                <span className="gray">{props.data.location}</span>       
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>       
             </div>    
             <p>{props.title}</p> 
-            <p><span className="bold">From {props.data.price}</span> / person</p>   
+            <p><span className="bold">From {props.price}</span> / person</p>   
         </div>
     )
 }
